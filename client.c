76885ce13696127640005ac84ff4bbab5ff1ab1b
Pierre Belabbes <pbelabbes@gmail.com>
2017-04-19 21:16:57 +0200

    Client.c : mise en place de la boucle de jeu

--- client.c
+++ client.c
@@ -12,7 +12,7 @@
 
 #include <time.h>
 #include <stdio.h>
-#include <curses.h> 		/* Primitives de gestion d'�cran */
+#include <curses.h> 		/* Primitives de gestion d'残ran */
 #include <sys/signal.h>
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -28,16 +28,18 @@ void client_appli (char *serveur, char *service);
 static void purger(void);
 void afficher_tab(char*);
 void start_client(char*);
-int level_choice();
+void level_choice();
 void remplir_tab_user(char*);
 
+int TAB_SIZE;
 
 
 /*****************************************************************************/
 /*--------------- programme client -----------------------*/
 
 void afficher_tab(char* tab){
-	for (int i = 0; i <4; ++i)
+  int i;	
+  for (i = 0; i <TAB_SIZE; ++i)
 	{
 		printf(" tab [%d] = %c\n",i,tab[i] );
 	}
@@ -51,21 +53,30 @@ static void purger(void)
     {}
 }
 
-int level_choice(){
+void level_choice(){
 	int level;
 	printf("Choose your level : 1 - Beginner  / 2- Medium / 3- Hard\n");
 	scanf("%d",&level);
-	if (level<1 || level>3)
-	{
-		return(level_choice());
+	switch(level){
+	  
+	case 1 : 
+	  TAB_SIZE = 4;
+	  break;
+	case 2 :
+	  TAB_SIZE = 5;
+	  break;
+	case 3 : 
+	  TAB_SIZE = 6;
+	  break;
+	default : 
+	  level_choice();
 	}
-	else
-		return level;
 }
 
 void remplir_tab_user(char* tab){
 	char inserer=0;
-	for (int i = 0; i <4; ++i)
+	int i;
+	for (i = 0; i < TAB_SIZE; ++i)
 	{	printf("Choisissez la %dieme couleur ",i);
 		scanf("%c",&inserer);
 		purger();
@@ -74,11 +85,20 @@ void remplir_tab_user(char* tab){
 }
 
 void start_client(char* tab_user)
-{	int level;
-	level=level_choice();
+{      
+	level_choice();
 	purger();
 	remplir_tab_user(tab_user);
 }
+
+int check_tab(char* tab_answer){
+  int i;
+  for (i=0 ; i<TAB_SIZE;i++ ){
+    if ( tab_answer[i] != 'r') return 0;
+  }
+  return 1;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -115,52 +135,74 @@ int main(int argc, char *argv[])
 }
 
 /*****************************************************************************/
-void client_appli (char *serveur,char *service)
-
-/* procedure correspondant au traitement du client de votre application */
-
-{int to_server_socket = -1;
+void client_appli (char *serveur,char *service) /* procedure correspondant au traitement du client de votre application */
+{
+  int to_server_socket = -1;
   char *server_name = serveur;
-struct sockaddr_in serverSockAddr;
-struct hostent *serverHostEnt;
-long hostAddr;
-long status;
-char buffer[512];
-
-
-bzero(&serverSockAddr,sizeof(serverSockAddr));
-hostAddr = inet_addr(serveur);
-
-serverSockAddr.sin_port = htons(30000);
-serverSockAddr.sin_family = AF_INET;
-inet_aton(server_name, &serverSockAddr.sin_addr.s_addr);
+  struct sockaddr_in serverSockAddr;
+  struct hostent *serverHostEnt;
+  long hostAddr;
+  long status;
+  char buffer[512];
+  
+  
+  bzero(&serverSockAddr,sizeof(serverSockAddr));
+  hostAddr = inet_addr(serveur);
+  
+  serverSockAddr.sin_port = htons(30000);
+  serverSockAddr.sin_family = AF_INET;
+  inet_aton(server_name, &serverSockAddr.sin_addr.s_addr);
 /* creation de la socket */
-if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
-{
-  printf("Echec creation socket client\n");
-  exit(0);
-}
-/* requete de connexion */
-if(connect( to_server_socket,
-            (struct sockaddr *)&serverSockAddr,
-            sizeof(serverSockAddr)) < 0 )
-{
-  printf("Echec demande de connection\n");
-  exit(0);
-}
-char* tab_user=malloc(sizeof(char)*4);
-start_client(tab_user);
-/* envoie de donne et reception */
-write(to_server_socket,tab_user,sizeof(tab_user));
+  if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
+    {
+      printf("Echec creation socket client\n");
+      exit(0);
+    }
+  /* requete de connexion */
+  if(connect( to_server_socket, (struct sockaddr *)&serverSockAddr, sizeof(serverSockAddr)) < 0 )
+    {
+      printf("Echec demande de connection\n");
+      exit(0);
+    }
 
-// r�ception du nombre de couleurs trouv�s
-int nbr_valides;
-recv(to_server_socket,&nbr_valides, sizeof nbr_valides, 0);
-printf("Vous avez reussi a trouver %d batonnets\n",nbr_valides );
-//printf("%s",buffer);
-/* fermeture de la connection */
-h_shutdown(to_server_socket,2);
-h_close(to_server_socket);
+  // JEU
+  char* tab_user=malloc(sizeof(char)*TAB_SIZE);
+  start_client(tab_user); // Récupère le niveau de difficulté et la grille de départ du joueur
+ 
+  char* tab_answer = malloc(sizeof(char)*TAB_SIZE); // Crée la grille dans laquelle seront reçus les réponses
+  int gagner = 0; // booléen de victoire
+  
+  /* envoie de la taille de la grille */
+  char tab_size_data[1] = {(char) TAB_SIZE};
+  h_writes(to_server_socket,tab_size_data,sizeof(tab_size_data));
+  
+  /* envoie de donne et reception */
+  h_writes(to_server_socket,tab_user,sizeof(tab_user));
+  
+  // réception du nombre de couleurs trouvé
+  h_reads(to_server_socket,tab_answer, sizeof tab_answer);
+
+   
+  while(!gagner){
+    gagner = check_tab(tab_answer);
+    printf("La table de réponse est la suivante : \n");
+    afficher_tab(tab_answer);
+    printf("r : bonne position, w : présent dans la grille mais mauvaise position\n");
+    remplir_tab_user(tab_user);
+    
+    /* envoie de donne et reception */
+    h_writes(to_server_socket,tab_user,sizeof(tab_user));
+    
+    // réception du nombre de couleurs trouvé
+    h_reads(to_server_socket,tab_answer, sizeof tab_answer);
+    
+  }
+
+  printf("Féliciation vous avez gagné !");
+
+  /* fermeture de la connection */
+  h_shutdown(to_server_socket,2);
+  h_close(to_server_socket);
 }
 /*****************************************************************************/
 
