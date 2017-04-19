421dd6ea8e6be1c97783315b34454acf14500908
titouan <larnicol.titouan@orange.fr>
2017-04-19 12:59:30 +0200

    mastermind local

--- client.c
+++ client.c
@@ -10,23 +10,75 @@
 /*									      */
 /******************************************************************************/	
 
-
+#include <time.h>
 #include <stdio.h>
 #include <curses.h> 		/* Primitives de gestion d'Žcran */
 #include <sys/signal.h>
 #include <sys/wait.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
 #include "fon.h"   		/* primitives de la boite a outils */
 
 #define SERVICE_DEFAUT "1111"
 #define SERVEUR_DEFAUT "127.0.0.1"
 
+
 void client_appli (char *serveur, char *service);
+static void purger(void);
+void afficher_tab(char*);
+void start_client(char*);
+int level_choice();
+void remplir_tab_user(char*);
+
 
 
 /*****************************************************************************/
 /*--------------- programme client -----------------------*/
+
+void afficher_tab(char* tab){
+	for (int i = 0; i <4; ++i)
+	{
+		printf(" tab [%d] = %c\n",i,tab[i] );
+	}
+}
+
+static void purger(void)
+{
+    int c;
+
+    while ((c = getchar()) != '\n' && c != EOF)
+    {}
+}
+
+int level_choice(){
+	int level;
+	printf("Choose your level : 1 - Beginner  / 2- Medium / 3- Hard\n");
+	scanf("%d",&level);
+	if (level<1 || level>3)
+	{
+		return(level_choice());
+	}
+	else
+		return level;
+}
+
+void remplir_tab_user(char* tab){
+	char inserer=0;
+	for (int i = 0; i <4; ++i)
+	{	printf("Choisissez la %dieme couleur ",i);
+		scanf("%c",&inserer);
+		purger();
+		*(tab+i)=inserer;
+	}
+}
+
+void start_client(char* tab_user)
+{	int level;
+	level=level_choice();
+	purger();
+	remplir_tab_user(tab_user);
+}
 
 int main(int argc, char *argv[])
 {
@@ -67,12 +119,44 @@ void client_appli (char *serveur,char *service)
 
 /* procedure correspondant au traitement du client de votre application */
 
+{int to_server_socket = -1;
+  char *server_name = serveur;
+struct sockaddr_in serverSockAddr;
+struct hostent *serverHostEnt;
+long hostAddr;
+long status;
+char buffer[512];
+
+
+bzero(&serverSockAddr,sizeof(serverSockAddr));
+hostAddr = inet_addr(serveur);
+
+serverSockAddr.sin_port = htons(30000);
+serverSockAddr.sin_family = AF_INET;
+inet_aton(server_name, &serverSockAddr.sin_addr.s_addr);
+/* creation de la socket */
+if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
 {
-  
-
-/* a completer .....  */
-
- }
-
+  printf("Echec creation socket client\n");
+  exit(0);
+}
+/* requete de connexion */
+if(connect( to_server_socket,
+            (struct sockaddr *)&serverSockAddr,
+            sizeof(serverSockAddr)) < 0 )
+{
+  printf("Echec demande de connection\n");
+  exit(0);
+}
+char* tab_user=malloc(sizeof(char)*4);
+start_client(tab_user);
+/* envoie de donne et reception */
+write(to_server_socket,tab_user,sizeof(tab_user));
+//read(to_server_socket,buffer,512);
+//printf("%s",buffer);
+/* fermeture de la connection */
+h_shutdown(to_server_socket,2);
+h_close(to_server_socket);
+}
 /*****************************************************************************/
 
--- serveur.c
+++ serveur.c
@@ -11,22 +11,47 @@
 /*									      */
 /******************************************************************************/	
 
-#include<stdio.h>
+#include <time.h>
+#include <stdio.h>
 #include <curses.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include<sys/signal.h>
-#include<sys/wait.h>
-#include<stdlib.h>
+
 
 #include "fon.h"     		/* Primitives de la boite a outils */
 
 #define SERVICE_DEFAUT "1111"
+void init_tab_find(char*);
+static void purger1(void);
+void afficher_tab1(char*);
+int jouer_tour(char*,char*);
+int partie(char*);
 
 void serveur_appli (char *service);   /* programme serveur */
 
 
+char buffer[512];
+int ma_socket;
 /******************************************************************************/	
 /*---------------- programme serveur ------------------------------*/
+void afficher_tab1(char* tab){
+	for (int i = 0; i <4; ++i)
+	{
+		printf(" tab [%d] = %c\n",i,tab[i] );
+	}
+}
+
+static void purger1(void)
+{
+    int c;
+
+    while ((c = getchar()) != '\n' && c != EOF)
+    {}
+}
+
 
 int main(int argc,char *argv[])
 {
@@ -63,9 +88,124 @@ void serveur_appli(char *service)
 
 {
 
-/* A completer ... */
+int client_socket;
+struct sockaddr_in mon_address, client_address;
+int mon_address_longueur, lg;
 
+bzero(&mon_address,sizeof(mon_address));
+mon_address.sin_port = htons(30000);
+mon_address.sin_family = AF_INET;
+mon_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+/* creation de socket */
+if ((ma_socket = socket(AF_INET,SOCK_STREAM,0))== -1)
+{
+  printf("ca chie avec la creation\n");
+  exit(0);
 }
+/* bind serveur - socket */
+bind(ma_socket,(struct sockaddr *)&mon_address,sizeof(mon_address));
+
+/* ecoute sur la socket */
+listen(ma_socket,5);
+
+/* accept la connexion */
+mon_address_longueur = sizeof(client_address);
+
+client_socket = accept(ma_socket,(struct sockaddr *)&client_address,&mon_address_longueur);
+
+//JEU
+
+char* tab_user=malloc(sizeof(char)*4);
+ lg =read(client_socket,tab_user,sizeof(tab_user));
+ 	afficher_tab1(tab_user);
+ 	partie(tab_user);
+    //printf("le serveur a recu: %s\n",buffer);
+    //sprintf(buffer,"%s du serveur",buffer);
+    //write(client_socket,buffer, 512);
+    h_shutdown(client_socket,2);
+    h_close(client_socket);
+}
+
 
 /******************************************************************************/	
 
+
+
+void init_tab_find(char* tab){
+	 srand(time(NULL));
+	 char couleur[5]={'r','v','n','j','b'};
+	 int indice=0;
+	for (int i = 0; i <4; ++i)
+	{
+		indice= rand()%5;
+		printf("indice %d\n",indice);
+		*(tab+i)=couleur[indice];
+	}
+}
+
+
+
+
+int jouer_tour(char* tab_user,char* tab_to_find){
+	int nbr_valide=0;
+	int nbr_semi_valide=0;
+	int nbr_invalide=0;
+	int j=0;
+	/*do{	
+		for (int i = 0; i <4; ++i)
+		{			
+			if(tab_user[j]==tab_to_find[i] && i==j){
+				nbr_valide++;
+				printf("la couleur %c en position %d  est valide  et à la bonne place\n",tab_user[j],j );
+				j++;
+				i=4;
+			}
+			else if(tab_user[j]==tab_to_find[i] && i!=j){
+				nbr_semi_valide++;
+				printf("la couleur %c en position %d  est valide mais pas à la bonne place\n",tab_user[j],j );
+				j++;
+				i=4;
+			}
+			else
+				nbr_invalide++;
+		}
+	j++;
+	}while(j<4); */
+	for (int i = 0; i < 4; ++i)
+	{
+		if (tab_user[i]==tab_to_find[i])
+		{
+			printf("Le couleur position %d est bien place\n",i );
+			nbr_valide++;
+
+		}
+	}
+
+	if (nbr_valide==4)
+	{
+		return 1;
+	}
+	else
+		return 0;
+
+}
+
+
+
+
+int partie(char* tab_user)
+{	
+	char* tab_to_find=malloc(sizeof(char)*4);
+	init_tab_find(tab_to_find);
+	printf("(REPONSE= ");
+	afficher_tab1(tab_to_find);
+	printf(")");
+	jouer_tour(tab_user,tab_to_find);
+	printf("Vous avez gagne :)\n");
+	
+	free(tab_to_find);
+	
+
+	return 0;
+}
